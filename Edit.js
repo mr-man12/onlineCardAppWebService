eb1e7435f2ab64285feb233319b54b78fd963c95
hayden <hayden.shion@gmail.com>
2026-01-21 10:20:23 +0800

    L19 unversioned files

---
+++ Edit.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react';
+import { StatusBar, View, Button, Text, TextInput, Alert } from 'react-native';
+
+const Edit = ({navigation, route}) => {
+  const[name,setName] = useState("");
+  const[pic,setPic] = useState("");
+  return (
+    <View>
+      <StatusBar/>
+      <Text>Card Name:</Text>
+      <TextInput style={{borderWidth:1}} onChangeText={(text)=>setName(text)}/>
+      <Text>Card Pic URL:</Text>
+      <TextInput style={{borderWidth:1}} onChangeText={(text)=>setPic(text)}/>  
+      <Text> </Text>
+      <Button title='Update'
+      onPress={()=>{
+          
+        }
+      }
+      />
+      <Text> </Text>
+      <Button title='Delete'
+      onPress={()=>{
+          
+        }
+      }
+      />
+    </View>
+  );
+};
+
+export default Edit;
